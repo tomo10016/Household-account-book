4c3210fa4e19d5073cc67a154a9188ab75b038a2
tomo10016 <shinnyo.tomohiro276@mail.kyutech.jp>
2022-09-25 10:30:51 +0900

    20220925_01 commit

---
+++ resources/views/register.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Registration of items to purchase</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <body/>
+         <div class="items">
+            <h3>名目</h3>
+        </div>
+        <div class="bigcategory">
+            <h3>大カテゴリ</h3>
+        <div class="expenditure">
+            <h3>小カテゴリ</h3>
+        </div>
+        <div class="price">
+            <h3>値段</h3>
+        <div class="shop">
+            <h3>購入店舗</h3>
+        </div>
+        <div class="method">
+            <h3>支払方法</h3>
+        </div>
+    </body>
+</html>
